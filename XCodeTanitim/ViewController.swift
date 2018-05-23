a3495f606a31007669cb9057f8ed5db1065f619d
fatih acar <fatihacar@fatih-mac.local>
2018-05-23 14:13:36 +0300

    TableView myCell eklendi

--- XCodeTanitim/ViewController.swift
+++ XCodeTanitim/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ 
+    var arabalar = ["Bmw","Mercedes","Audi","Volvo"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arabalar.count
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = arabalar[indexPath.row]
+        return cell
+    }
 
 }
 
